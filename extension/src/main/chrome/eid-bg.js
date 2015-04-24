12ec8d09b76cf47e7c5e514d8ac8f906044a7a31
egelke <info@egelke.net>
2015-04-24 09:58:12 +0200

    Restructure

--- extension/src/main/chrome/eid-bg.js
+++ extension/src/main/chrome/eid-bg.js
@@ -17,7 +17,7 @@ chrome.runtime.onConnect.addListener(function (port) {
     console.assert(port.name === "eid");
 
     this.ctPort = port;
-    //this.svPort = chrome.runtime.connectNative('net.egelke.eid');
+    this.svPort = chrome.runtime.connectNative('net.egelke.eid');
 
     this.ctPort.onMessage.addListener(this.onCtMsg);
     //this.svPort.onMessage.addListener(this.onSvMsg);
