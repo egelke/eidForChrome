75e327eb1026f4a802a7f86416458c02baf04d2a
Bryan Brouckaert <info@egelke.net>
2015-04-25 11:08:39 +0200

    Msg pass-through to page

--- extension/src/main/chrome/eid-bg.js
+++ extension/src/main/chrome/eid-bg.js
@@ -1,30 +1,30 @@
 
-function onSvMsg(msg) {
-
+function onNtMsg(msg) {
+    console.log('eid-bg: native message');
+    console.dir(msg);
+    
+    this.ctPort.postMessage(msg);
 }
 
 function onCtMsg(msg) {
-    console.log('processing content message');
+    console.log('eid-bg: content message');
     console.dir(msg);
-    
+
     if (msg.action === "start") {
         chrome.pageAction.show(this.ctPort.sender.tab.id);
-        this.ctPort.postMessage({action: "initializing"});
-        
-        this.svPort = chrome.runtime.connectNative('net.egelke.chrome.eid.native');
-        this.svPort.onMessage.addListener(onSvMsg);
 
-        this.svPort.postMessage(msg);
+        this.svPort = chrome.runtime.connectNative('net.egelke.chrome.eid.native');
+        this.svPort.onMessage.addListener(onNtMsg);
     }
+    
+    this.svPort.postMessage(msg);
 }
 
-//Listen to onConnect. Warnin! Run in content scope
+
 chrome.runtime.onConnect.addListener(function (port) {
     console.assert(port.name === "eid");
 
     this.ctPort = port;
     this.ctPort.onMessage.addListener(onCtMsg);
-    //this.svPort.onMessage.addListener(this.onSvMsg);
-
 });
 
--- extension/src/main/chrome/eid-ct.js
+++ extension/src/main/chrome/eid-ct.js
@@ -1,21 +1,26 @@
 
 var Eid = (function () {
 
-    function Eid (element) {
-        this.e = element;
+    function Eid () {
         this.port = chrome.runtime.connect({name: "eid"});
-        
-        this.port.onMessage.addListener(this.onMsg);
-        this.port.postMessage({
-            action: "start",
-            params: {}
-        });
+        this.port.onMessage.addListener(this.bgMsg);
     }
     
-    Eid.prototype.onMsg = function (msg) {
-        if (msg.action === "initializing") {
-            this.e.innerHTML = "<span>initializing...</span>";
+    Eid.prototype.bgMsg = function (msg) {
+        console.log('eid-ct: Background message');
+        console.dir(msg); 
+        
+        if (msg.action === 'UI') {
+            //TODO
         }
+        window.postMessage(msg, "*");
+    };
+    
+    Eid.prototype.pgMsg = function (msg) {
+        console.log('eid-ct: Page message');
+        console.dir(msg);
+        
+        this.port.postMessage(msg);
     };
     
     
@@ -23,7 +28,16 @@ var Eid = (function () {
 
 })();
 
+function pgEvent(event) {
+    if (event.source !== window || event.data.type !== 'eid-req') return;
+    
+    eid.pgMsg(event.data);
+}
+
+var eid;
 var e = document.getElementById("eid");
 if (e !== null) {
-    var eid = new Eid(e);
+    eid = new Eid();
+    window.addEventListener("message", pgEvent, false);
+    this.e.innerHTML = "ready";
 };
